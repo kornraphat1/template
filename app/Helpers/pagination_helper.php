ec92c29c143508889fefc9f140f77f5e908028bd
totolove1126 <to_patipat@hotmail.com>
2021-02-05 19:45:38 +0700

    toez

--- app/Helpers/pagination_helper.php
+++ app/Helpers/pagination_helper.php
@@ -1,18 +1,12 @@
 <?php
-use App\Models\Video_Model;
-
-
-
     function get_pagination($sql,$page,$perpage,$total){
-
         $db = \Config\Database::connect();
         $start = ((int)$page - 1) * (int)$perpage;
-
         $sql .= " LIMIT ?, ?";
         $query = $db->query($sql,[$start,$perpage]);
         $result = $query->getResultArray();
+        
         $total_page = ceil((int)$total/(int)$perpage);
-
         $data = [
             'list' => $result,
             'start_row' => $start+1,
@@ -21,112 +15,85 @@ use App\Models\Video_Model;
             'total_page' => $total_page,
             'perpage' => $perpage
         ];
-
         return $data;
     }
-
     function pagination($page, $total_page){
-
+        
         $html = "";
         $link = explode('?',$_SERVER['REQUEST_URI']);
         $link = $link[0];
         $pre = $page; $pagepre = $page; $predisable = '';
         $pagenext = $page+1; $nextdisable = '';
         $totals = $total_page;
-
         if($page <= 1 ){
-
             $page = 1;
             $pre = 1;
             $pagepre = 1;
             $pagenext = $page+1;
-            $predisable = 'disabled';
-
-
+            //$predisable = 'disabled';
             if($total_page>5){
                 $total_page = 5;
             }
-
+            // if( $page >= $total_page ){
+            //     $nextdisable = 'disabled';
+            // }
+            
         }else if( $page >= $total_page ){
-
             $pagepre = $total_page - 4;
             $page = $pagenext = $total_page;
-            $nextdisable = 'disabled';
-
+            //$nextdisable = 'disabled';
             if($total_page<=5){
-
                 $pagepre = 1;
                 $pre = $page-1; 
-
             }
-
         }else if(($page%5)==0){
-
             $pre = $page-1;
             $pagepre = $page;
             $pagenext = $page+1;
-
             if($total_page>=($pagepre+4)){
                 $total_page = $pagepre + 4;
             }
-
+            
         }else{
-
             $pre = $page-1;
             $pagepre = $page;
             $pagenext = $page+1;
-
-            if( $total_page >= '5' && $total_page >= ($page+4) ){
-                
+            
+            if( $total_page >= 5 ){
                 $total_page = $page+4;
-                
             }else if( $total_page < 5 ){
-                
                 $pagepre = 1;
-                
             }
-
+            
         }
-
-        $html .= '<ul class="paginator">';
-            $html .= '<li class="paginator__item '.$predisable.'"><a href="'.$link.'?page=1" >หน้าแรก</a></li>';
-
+        
+        $html .= '<ul class="pagination dropshd">';
+            $html .= '<li class="'.$predisable.'"><a href="'.$link.'?page=1" >หน้าแรก</a></li>';
             if(empty($predisable)){
-
-                $html .= '<li class="paginator__item paginator__item--prev"><a href="'.$link.'?page='.$pre.'">&lsaquo;</a></li>';
-
+                $html .= '<li><a href="'.$link.'?page='.$pre.'" >&lsaquo;</a></li>';
             }else{
-
-                $html .= '<li class="paginator__item paginator__item--prev '.$predisable.'"><a href="#">&lsaquo;</a></li>';
-
+                $html .= '<li class="'.$predisable.'"><a href="#" >&lsaquo;</a></li>';
             }
-
             for($i=$pagepre; $i<=$total_page; $i++){
-
                 $active='';
                 if($page==$i){
-                    $active=' active';
+                    $active='class="active"';
                 }
-
-                $html .= '<li class="paginator__item'.$active.'"><a href="'.$link.'?page='.$i.'">'.$i.'</a></li>';
-
+                $html .= '<li '.$active.'><a href="'.$link.'?page='.$i.'">'.$i.'</a></li>';
+                // $check = $page+5;
+                // if($total_page>=10 && $i == $check){
+                //     $html .= '<li class="disabled"><span class="legitRipple">...</span></li>';
+                //     $i = $total_page - 3; 
+                // }
             }
-
             if(empty($nextdisable)){
-
-                $html .= '<li class="paginator__item paginator__item--next"><a href="'.$link.'?page='.$pagenext.'" >&rsaquo;</a></li>';
-
+                $html .= '<li><a href="'.$link.'?page='.$pagenext.'" >&rsaquo;</a></li>';
             }else{
-
-                $html .= '<li class="paginator__item '.$nextdisable.'"><a href="#" >&rsaquo;</a></li>';
-
+                $html .= '<li class="'.$nextdisable.'"><a href="#" >&rsaquo;</a></li>';
             }
-
-            $html .= '<li class="paginator__item '.$nextdisable.'"><a href="'.$link.'?page='.$totals.'" >หน้าสุดท้าย</a></li>';
+            $html .= '<li class="'.$nextdisable.'"><a href="'.$link.'?page='.$totals.'" >หน้าสุดท้าย</a></li>';
         $html .= '</ul>';
-
+        
         return $html;
-
     }
-
 ?>
--- app/Views/movie/MV-2/left.php
+++ app/Views/movie/MV-2/left.php
@@ -1,23 +1,23 @@
 <div class="sec_main-left">
-           <div class="cate">
-               <div class="h3-text text-cat" style="background-color: crimson; padding-bottom: 1px;">
-                   <h3>หมวดหมู่</h3>
-               </div>
-               <div class="grid-cate">
-                   <ul>
-                       <li id="nav_menu-4" class="widget widget_nav_menu">
-                           <div class="menu-cate-menu-container">
-                               <ul id="menu-cate-menu" class="menu">
-                                   <li id="menu-item-38" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-38 list-cate">
-                                       <?php foreach ($category_list as $value) { ?>
-                                           <h3><a class=" list-cate" title=<?= $value['category_name'] ?> alt=<?= $value['category_name'] ?> href="<?php echo base_url('/category/' . $value['category_id'] . '/' . urlencode(str_replace(' ', '-', trim($value['category_name'])))); ?>"> <?= $value['category_name'] ?></a></h3>
-                                       <?php } ?>
-                                   </li>
-                               </ul>
-                           </div>
-                       </li>
-                       <li id="search-8" class="widget widget_search"></li>
-                   </ul>
-               </div>
-           </div>
-       </div> 
+    <div class="cate">
+        <div class="h3-text text-cat" style="background-color: crimson; padding-bottom: 1px;">
+            <h3>หมวดหมู่</h3>
+        </div>
+        <div class="grid-cate">
+            <ul>
+                <li id="nav_menu-4" class="widget widget_nav_menu">
+                    <div class="menu-cate-menu-container">
+                        <ul id="menu-cate-menu" class="menu">
+                            <li id="menu-item-38" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-38 list-cate">
+                                <?php foreach ($category_list as $value) { ?>
+                                    <h3><a class=" list-cate" title=<?= $value['category_name'] ?> alt=<?= $value['category_name'] ?> href="<?php echo base_url('/category/' . $value['category_id'] . '/' . urlencode(str_replace(' ', '-', trim($value['category_name'])))); ?>"> <?= $value['category_name'] ?></a></h3>
+                                <?php } ?>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li id="search-8" class="widget widget_search"></li>
+            </ul>
+        </div>
+    </div>
+</div>
